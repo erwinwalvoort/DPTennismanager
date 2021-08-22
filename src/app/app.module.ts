8da925e911accc82347303a74341641468959643
DePilleIT <info@dp-it.nl>
2021-08-22 21:17:05 +0200

    ionic added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 //import { FCM } from '@ionic-native/fcm/ngx';
-import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated';
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
